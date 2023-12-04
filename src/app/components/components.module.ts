92c0c29f5630143d468d28c212868dc3d88d3e5f
Jonas Willian <jwnarancity@gmail.com>
2023-12-04 13:49:44 -0300

    Criação do componente input

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './card/card.component';
+import { InputComponent } from './input/input.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    InputComponent
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   exports:[
-    CardComponent
+    CardComponent,
+    InputComponent,
   ]
   
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
